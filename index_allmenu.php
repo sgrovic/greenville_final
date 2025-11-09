2ee585c2279d916c83a98b6527f3ae238c3bee53
Rovic <sangabrielrovic@gmail.com>
2025-11-09 23:09:15 +0800

    Added viewport to support scaling for mobile web browsers

--- index_allmenu.php
+++ index_allmenu.php
@@ -15,6 +15,7 @@ $users = [
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enrollment System</title>
 
     <h2 id="welcome"></h2>
